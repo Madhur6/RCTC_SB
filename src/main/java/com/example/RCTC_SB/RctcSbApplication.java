85fad38779aa21eaefb11b21ff4d10a966733a11
Madhur6 <madhur.20eic525@rtu.ac.in>
2024-11-13 21:16:33 +0530

    First Proper Commit

---
+++ src/main/java/com/example/RCTC_SB/RctcSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.RCTC_SB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RctcSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RctcSbApplication.class, args);
+	}
+
+}
